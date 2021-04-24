c3ed5132b119de4ec5f5bfd53e8e332e40a93984
layersony <sammaingi5@gmail.com>
2021-04-24 11:56:09 +0300

    Add page loader

--- js/script.js
+++ js/script.js
@@ -166,3 +166,13 @@ $(window).on("load", function () {
     $("#toadd").toggle(500);
   });
 });
+
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector("body").style.visibility = "hidden";
+      document.querySelector("#loader").style.visibility = "visible";
+   } else {
+      document.querySelector("#loader").style.display = "none";
+      document.querySelector("body").style.visibility = "visible";
+  }
+};
